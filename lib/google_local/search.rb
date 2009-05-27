00ca8090ac3f7a3802c1ca8e97797573cac47c28
Josh Owens <joshua.owens@gmail.com>
2009-05-27 10:31:47 -0400

    Some comments.

--- lib/google_local/search.rb
+++ lib/google_local/search.rb
@@ -1,20 +1,30 @@
 module GoogleLocal
   
+  # A search sent to Google Local Search RESTful API
   class Search
     include HTTParty
     
     base_uri "http://ajax.googleapis.com/ajax/services/search"
     default_params :v => '1.0'
-    attr_accessor :center_location, :api_key, :latlng
+    
+    # Central location for the local search
+    attr_accessor :center_location
+    
+    # Google Search API Key
+    attr_accessor :api_key
+    
+    # The latitude and longitude of the central location
+    attr_accessor :latlng
     
     def initialize(center_location, api_key=nil)
       @api_key = api_key
-      @latlng = GoogleLocal::Geocode.find_latlng(center_location)
+      @latlng = GoogleLocal::Geocode.find_latlng(center_location) # Parse center_location into latitude and longitude
     end
     
+    # Perform a Google Local search given a query.
     def find(query,options={})
-      options.merge!(:q => query, :sll => @latlng)
-      fetch_locations(self.class.get("/local", :query => options))
+      options.merge!(:q => query, :sll => @latlng) # Merge in the query and the lat/lng into the options
+      fetch_locations(self.class.get("/local", :query => options)) # Make the get request
     end
     
     private
