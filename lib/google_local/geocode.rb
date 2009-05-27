6ec6f561d2cfceca7675ab1a9a8fe6a723784055
Josh Owens <joshua.owens@gmail.com>
2009-05-27 10:33:41 -0400

    More comments

--- lib/google_local/geocode.rb
+++ lib/google_local/geocode.rb
@@ -1,13 +1,15 @@
 module GoogleLocal
   
+  # Geocoding to fetch the latitude/longitude of a location string
   class Geocode
     include Geokit::Geocoders
     Geokit::Geocoders::provider_order = [:google,:us]
     Geokit::Geocoders::google = MAPS_API_KEY
     
+    # A request to the geocoding service to fetch the latitude/longitude of a location string
     def self.find_latlng(location)
-      res=MultiGeocoder.geocode(location)
-      res.ll
+      res=MultiGeocoder.geocode(location) # Get the geocoding response
+      res.ll # Return the lat/lng
     end
     
   end
